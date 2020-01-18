8057c5086ba269fa4f14b93046c49fce2cba884e
Amit Kumar Biswas <60037798+amit2x01@users.noreply.github.com>
2020-01-18 19:52:28 +0530

    Add files via upload

---
+++ chase-board.php
@@ -0,0 +1,74 @@
+<style type="text/css" media="screen">
+	*{margin: 0;padding: 0;box-sizing: border-box;}
+	body{
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	.square{
+		box-sizing: border-box;
+		width: 50px;
+		display: inline-block;
+		height: 50px;
+		border:2px solid #232222;
+		margin: 0;padding: 0;
+	}
+	.sq-black{
+		background-color:#2a2a2a;
+	}
+</style>
+
+<div class="chess">
+<?php 
+
+
+
+for ($i=0; $i < 8; $i++) { 
+
+	if($i % 2 != 0){
+		for ($j=0; $j < 8; $j++) { 
+
+			if($j % 2 == 0){
+				echo  '<div class="square sq-black"></div>';
+			}else{
+				echo  '<div class="square"></div>';
+			}
+		
+		}	
+	}else{
+		for ($j=0; $j < 8; $j++) { 
+			
+			if($j % 2 == 0){
+				echo  '<div class="square"></div>';
+			}else{
+				echo  '<div class="square sq-black"></div>';
+			}
+
+		}
+	}
+
+	echo '<br>';
+	
+}
+
+
+
+
+
+
+
+
+
+
+ ?>
+
+<!-- 	
+ <div class="square sq-black"></div>
+ <div class="square"></div>
+ <div class="square sq-black"></div>
+ <br>
+ <div class="square"></div>
+ <div class="square sq-black"></div>
+ <div class="square"></div> -->
+
+</div>
